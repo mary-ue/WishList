4ac946ffba50216685740e4df70271ab7500ec2e
Mary <maryandreeva100@gmail.com>
2023-05-25 22:20:54 +0300

    authorization

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,62 @@
 import { renderNavigation } from "./renderNavigation.js";
 import { createHero } from "./createHero.js";
+import { createWishlist } from "./createWishlist.js";
+import { getLogin } from "./getLogin.js";
+import { JWT_TOKEN_KEY } from "./const.js";
 
+export const router = Router();
+const token = localStorage.getItem(JWT_TOKEN_KEY);
+export const auth = token ? await getLogin(token) : {};
 
 const app = document.querySelector('.app');
+
+const handleEditPageRoute = (id) => {
+
+}
+
+const handleEditProfileRoute = (login) => {
+
+}
+
+const handleUserRoute = async (login) => {
+  app.textContent = ''; 
+  renderNavigation();
+  app.append(await createWishlist(login));
+}
+
+createWishlist()
 
 //  Вызвать загрузку главной страницы
 const handleHomePage = () => {
   app.textContent = ''; // каждый раз при загрузке очищаем приложение
 
   renderNavigation();
-  const section = createHero();
   app.append(createHero());
 }
 
 /* Инициализация приложения */
 const init = () => {
-  handleHomePage();
-}
+  let isMainPage = true;
+
+  router.on('/', handleHomePage);
+
+  router.on('/editwish/newwish', handleEditPageRoute);
+  router.on('/editwish/:id', handleEditPageRoute);
+  router.on('/editprofile/:ligin', handleEditProfileRoute);
+  router.on('/user/:login', handleUserRoute);
+
+  router.init();
+
+  if (isMainPage) {
+    isMainPage = false;
+    router.setRoute('/');
+
+    if(auth.login) {
+      router.setRoute(`/user/${auth.login}`);
+    } else {
+      router.setRoute('/')
+    }
+  }
+};
+
 init();
--- scripts/renderNavigation.js
+++ scripts/renderNavigation.js
@@ -1,7 +1,11 @@
+import { API_URL, JWT_TOKEN_KEY } from './const.js';
+import { createBurgerMenu } from "./createBurgerMenu.js";
 import { createElement } from "./helper.js";
+import { auth, router } from './index.js';
+import { renderModal } from './renderModal.js';
 
 const nav = document.querySelector('.nav');
-// const burger = createBurgerMenu(nav);
+createBurgerMenu(nav, 'nav_active', '.nav__btn');
 
 export const renderNavigation = () => {
   nav.textContent = '';
@@ -12,7 +16,43 @@ export const renderNavigation = () => {
   });
 
   buttonSignUp.addEventListener('click', () => {
-    console.log('Зарегистрироваться')
+    renderModal({
+      title: 'Регистрация', 
+      description: 'Введите ваши данные для регистрации на сервисе WishList',
+      btnSubmit: 'Зарегистрироваться',
+      async submitHandler(event) {
+        const formData = new FormData(event.target);
+        const credentials = {
+          login: formData.get('login'),
+          password: formData.get('password'),
+        };
+        console.log(credentials)
+
+        try {
+          const response = await fetch(`${API_URL}/register`, {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify(credentials)
+          });
+
+          if(response.ok) {
+            const data = await response.json();
+            console.log(data);
+            localStorage.setItem(JWT_TOKEN_KEY, data.token);
+            auth.login = data.login;
+            router.setRoute(`/user/${data.login}`);
+
+            return true;
+          } else {
+            const { message = 'Неизвестная ошибка' } = await response.json();
+            console.log(message);
+            throw new Error(message);
+          }
+        } catch (error) {
+          console.log(error.message)
+        }
+      }
+    })
   });
 
   const buttonLogin = createElement('button', {
